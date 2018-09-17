d1f0629d2e5cf3dc5e4d58cb68046b3d4e7c6201
Ved Sachdeva <ved.sachdeva@walmart.com>
2018-09-17 22:54:00 +0530

    removed unused import

--- src/main/java/com/thefallenbrain/stayfithub/services/domain/WorkoutExerciseProjection.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/domain/WorkoutExerciseProjection.java
@@ -2,8 +2,6 @@ package com.thefallenbrain.stayfithub.services.domain;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.sun.xml.internal.ws.api.pipe.FiberContextSwitchInterceptor;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "flat", types = WorkoutExercise.class)
