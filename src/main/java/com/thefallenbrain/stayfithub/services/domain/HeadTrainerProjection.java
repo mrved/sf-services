bf7b944c529c9452988c0fed803fb82691cfa188
Ved Sachdeva <ved.sachdeva@walmart.com>
2018-10-14 20:42:58 +0530

    update

--- src/main/java/com/thefallenbrain/stayfithub/services/domain/HeadTrainerProjection.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/domain/HeadTrainerProjection.java
@@ -2,7 +2,45 @@ package com.thefallenbrain.stayfithub.services.domain;
 
 import org.springframework.data.rest.core.config.Projection;
 
+import java.util.Date;
+
 @Projection(name = "flat", types = HeadTrainer.class)
 public interface HeadTrainerProjection {
 	FitnessCenterProjection getFitnessCenter();
+	Integer getId();
+
+	String getName();
+
+	String getEmail();
+
+	String getPassword();
+
+	String getGender();
+
+	String getDesignation();
+
+	String getPhone();
+
+	Date getDob();
+
+	String getProfilePicture();
+
+	Double getHeight();
+
+	Double getWeight();
+
+	Double getBmi();
+
+	String getUsername();
+
+	boolean isAccountExpired();
+
+	boolean isAccountLocked();
+
+	boolean isCredentialsExpired();
+
+	boolean isEnabled();
+
+
+	String getMasterPassword();
 }
--- src/main/java/com/thefallenbrain/stayfithub/services/domain/TrainerProjection.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/domain/TrainerProjection.java
@@ -7,5 +7,43 @@ import java.util.Date;
 @Projection(name = "flat", types = Trainer.class)
 public interface TrainerProjection {
 	Date getDoj();
+
 	FitnessCenterProjection getFitnessCenter();
+
+	Integer getId();
+
+	String getName();
+
+	String getEmail();
+
+	String getPassword();
+
+	String getGender();
+
+	String getDesignation();
+
+	String getPhone();
+
+	Date getDob();
+
+	String getProfilePicture();
+
+	Double getHeight();
+
+	Double getWeight();
+
+	Double getBmi();
+
+	String getUsername();
+
+	boolean isAccountExpired();
+
+	boolean isAccountLocked();
+
+	boolean isCredentialsExpired();
+
+	boolean isEnabled();
+
+
+	String getMasterPassword();
 }
