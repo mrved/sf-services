80b71d2fd96e7066436d89c2b5a68eed8145b7dc
Ved Sachdeva <ved.sachdeva@walmart.com>
2018-10-15 15:45:34 +0530

    update

--- src/main/java/com/thefallenbrain/stayfithub/services/controller/PaymentController.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/controller/PaymentController.java
@@ -3,14 +3,18 @@ package com.thefallenbrain.stayfithub.services.controller;
 import com.thefallenbrain.stayfithub.services.domain.Member;
 import com.thefallenbrain.stayfithub.services.domain.Membership;
 import com.thefallenbrain.stayfithub.services.domain.MembershipType;
+import com.thefallenbrain.stayfithub.services.domain.Payment;
 import com.thefallenbrain.stayfithub.services.repository.MemberRepository;
 import com.thefallenbrain.stayfithub.services.repository.MembershipRepository;
 import com.thefallenbrain.stayfithub.services.repository.MembershipTypeRepository;
+import com.thefallenbrain.stayfithub.services.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class PaymentController {
@@ -22,6 +26,9 @@ public class PaymentController {
     @Autowired
     MembershipTypeRepository membershipTypeRepository;
 
+    @Autowired
+    PaymentRepository paymentRepository;
+
     @PostMapping(value = "subscription/success")
     @ResponseStatus(HttpStatus.OK)
     void subscription(@RequestParam Integer memberId,
@@ -31,6 +38,7 @@ public class PaymentController {
 
         MembershipType membershipType = membershipTypeRepository.findById(membershipTypeId).get();
         // setting up membership
+		Payment payment = new Payment();
         Membership membership = new Membership();
         membership.setStartDate(new Date());
         membership.setEndDate(afterDays(new Date(), membershipType.getDuration()));
@@ -39,8 +47,19 @@ public class PaymentController {
         membershipRepository.save(membership);
         member.setMembership(membership);
         member.setIsMembershipActive(true);
+
+
+        payment.setEndDate(membership.getEndDate());
+        payment.setStartDate(membership.getStartDate());
+        payment.setPaymentId(paymentId);
+        payment.setMemberId(memberId);
+        payment.setAmount(membershipType.getPrice());
+        payment.setMemberShipType(membershipType.getDescription());
+        payment.setMemberName(member.getName());
+        paymentRepository.save(payment);
         memberRepository.save(member);
     }
+
 
     Date afterDays(Date date, Long days){
         return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
--- src/main/java/com/thefallenbrain/stayfithub/services/domain/Payment.java
+++ src/main/java/com/thefallenbrain/stayfithub/services/domain/Payment.java
@@ -1,22 +1,26 @@
 package com.thefallenbrain.stayfithub.services.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
-
+    private String paymentId;
+    private Date startDate;
+    private Date endDate;
+    private String memberName;
+    private Integer memberId;
+    private String memberShipType;
     private double amount;
-
     private Date timestamp;
-
-    @OneToOne
-    private Membership membership;
-
-    @OneToOne
-    private Promotion promotion;
 
 }
